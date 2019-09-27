03a951b357dc1e495f3a26f655782ad0df40dfe2
Justin <justindchan@gmail.com>
2019-09-27 16:04:29 -0700

    deployment with load balancing and horizontal scaling

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const port = 3000;
 
 app.use('/listing/:id', express.static('public'))
 
-app.get('/', (req, res) => res.send('Hello World'))
+// app.get('/', (req, res) => res.send('Hello World'))
+
+// smart proxy has get request handler that then REDIRECTS to service server
+app.get('/api/photos/:propertyId', (req, res) => {
+  // redirect is absolute path to service server, taking in param to specify listing id
+  res.redirect(301, `http://ec2-54-219-149-8.us-west-1.compute.amazonaws.com/api/photos/${req.params.propertyId}`)
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}...`))
